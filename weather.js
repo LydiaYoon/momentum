d1f7f13a95cf1fcfda6ea9ec1685b3cefdf744cd
lydiayoon <narae456@gmail.com>
2019-12-15 18:42:07 +0900

    #3.9 Getting the Weather part One (Geolocation)

---
+++ weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "a9efa2e321b51a196743a2c9e70ecd58";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude; // 위도
+    const longitude = position.coords.longitude; // 경도
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+    console.log("Cant access geo location");
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCords = localStorage.getItem(COORDS);
+    if (loadedCords === null) {
+        askForCoords();
+    } else {
+        // getWeather
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
